c92a7dcf083b40d2ae8f9f013446527bd77863b9
Aravindhan V <aravindhan.v@perfios.com>
2021-11-10 18:11:50 +0530

    ADDED: input field with listenser to display customized message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      title: "Hello from bridgelabz"
+      userName: ''
     }
   }
 
@@ -16,12 +16,22 @@ class App extends React.Component {
     window.open(this.url, "_blank");
   }
 
+  onNameChange = (event) => {
+    console.log("value is: ", event.target.value);
+    this.setState({ userName: event.target.value });
+  }
+
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} alt="Bridgelabz logo" onClick={this.onClick} />
-      </div>
+      <>
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          <img src={logo} alt="Bridgelabz logo" onClick={this.onClick} />
+        </div>
+        <div>
+          <input onChange={this.onNameChange} />
+        </div>
+      </>
     )
   }
 }
