84c8f4238284e5ed82a7ab887030286a794befeb
Aravindhan V <aravindhan.v@perfios.com>
2021-11-10 18:00:21 +0530

    ADDED: click listener on image to open website in new tab

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import logo from './assets/img/logo.jpeg'
 
 class App extends React.Component {
+  url = "http://www.bridgelabz.com/"
   constructor() {
     super()
     this.state = {
       title: "Hello from bridgelabz"
     }
   }
+
+  onClick = ($event) => {
+    console.log("save button is clicked", $event);
+    window.open(this.url, "_blank");
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="Bridgelabz logo" />
+        <img src={logo} alt="Bridgelabz logo" onClick={this.onClick} />
       </div>
     )
   }
